c561375c743bf4e0f65592417bc99ca0b69725ae
imen341 <imen.benmansour17@outlook.fr>
2026-03-31 15:27:05 +0000

    your message

--- src/titanic/training/main.py
+++ src/titanic/training/main.py
@@ -8,13 +8,14 @@ from steps.split_train_test import split_train_test
 from steps.train import train
 
 def workflow(input_data_path: str, n_estimators: int, max_depth: int, random_state: int) -> None:
-  logging.warning(f"workflow input path : {input_data_path}")
-  local_path = load_data("all_titanic.csv")
-  xtrain_path, xtest_path, ytrain_path, ytest_path = split_train_test(local_path)
-  model_path = train(xtrain_path, ytrain_path, 100, 10, 42)
-  validate(model_path, xtest_path, ytest_path)
-  # TODO : Dans un second temps, démarrer le run mlflow au début de ce workflow
+    logging.warning(f"workflow input path : {input_data_path}")
+    local_path = load_data(input_data_path)
+    xtrain_path, xtest_path, ytrain_path, ytest_path = split_train_test(local_path)
+    model_path = train(xtrain_path, ytrain_path, n_estimators, max_depth, random_state)
+    validate(model_path, xtest_path, ytest_path)
+    # TODO : Dans un second temps, démarrer le run mlflow au début de ce workflow
 
 
 if __name__ == "__main__":
-  fire.Fire(workflow)
+    fire.Fire(workflow)
+    
